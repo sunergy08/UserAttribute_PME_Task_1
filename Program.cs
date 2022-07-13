cdad44d0206687c8a03c8730e27d960e6cb4c893
sunergy08 <65669659+sunergy08@users.noreply.github.com>
2022-07-13 03:37:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UserAttribute_PME_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            Console.WriteLine("Hello! What is you rname?");
+            name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Thank you " + name + "; you are " + age + " years old.");
+        }
+    }
+}
